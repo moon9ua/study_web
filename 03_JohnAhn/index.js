07df9d6cca1c4d813403e4715dbd9e16448143ae
moon9ua <koom6@naver.com>
2021-08-24 04:53:11 +0900

    modify: 03 폴더 구조 / add: 04

--- 03_JohnAhn/index.js
+++ 03_JohnAhn/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(
@@ -18,6 +25,17 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 정보를 client에서 가져오면, 데이터베이스에 넣어주기
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
